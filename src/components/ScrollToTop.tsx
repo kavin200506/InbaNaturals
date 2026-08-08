f932b8e25d3c623bb8dd6279d8aff73fd934447f
s-j-gokul-47 <gokulakannansj@gmail.com>
2026-08-08 09:10:54 +0530

    Made some more changes...

--- src/components/ScrollToTop.tsx
+++ src/components/ScrollToTop.tsx
@@ -1,11 +1,24 @@
 import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 
+declare global {
+  interface Window {
+    gtag?: (...args: any[]) => void;
+  }
+}
+
 export default function ScrollToTop() {
   const { pathname } = useLocation();
 
   useEffect(() => {
     window.scrollTo(0, 0);
+
+    // Send a pageview on route change for SPA tracking
+    if (typeof window.gtag === 'function') {
+      window.gtag('config', 'G-XXXXXXXXXX', {
+        page_path: pathname,
+      });
+    }
   }, [pathname]);
 
   return null;
